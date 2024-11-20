a22839163881eb828dc5fe8f9cb26bc96a5b410e
rmurton <africh84@gmail.com>
2024-11-20 17:14:12 +0000

    changes

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -6,9 +6,10 @@
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Nice to meet you, " << name << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
